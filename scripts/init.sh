6b7ace6064ff80f539eda62d15537b49f823860f
Todd Nine <tnine@apigee.com>
2016-09-30 16:27:50 -0600

    Updates each role to use it's own pet set.  This is due to current upgrade limitations in pet sets.
    Updates to be more declarative rather than automagic.

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,59 +1,6 @@
 #!/bin/bash
 
 
-function get_host_at_index(){
-  #Split the FQDN into parts based on the '.' char
-  IFS='.' read -ra hostcomponents <<< "$FQDN"
-
-  part=${hostcomponents[0]}
-
-  # echo "part is ${part}"
-
-  #First index, extract the hostname
-  output=${part%-*}
-  utput="${output}-${indexOut}"
-
-
-  echo $output
-
-
-}
-
-
-function get_fqdn_at_index(){
-  #Split the FQDN into parts based on the '.' char
-  IFS='.' read -ra hostcomponents <<< "$FQDN"
-
-  #Get all indexes, so we can iterate them and know the index
-  indexes=${!hostcomponents[*]}
-
-  # echo "Indexes is ${indexes}"
-
-  output=""
-  indexOut=$1
-
-  for index in $indexes;
-  do
-    part=${hostcomponents[$index]}
-
-    # echo "part is ${part}"
-
-    #First index, extract the hostname
-    if [ $index -eq 0 ]; then
-      output=${part%-*}
-      output="${output}-${indexOut}"
-    else
-      output="${output}.$part"
-    fi
-
-
-
-  done
-
-  echo $output
-
-
-}
 
 #Configure the master
 function configure_master() {
@@ -66,9 +13,8 @@ function configure_master() {
 
 
 
+  #TODO, we need to do an nslookup here
   allowed_replicas="10.244.0.0/16"
-  slave_node=$(get_fqdn_at_index 1)
-  slave_host=$(get_host_at_index 1)
 
   echo "Configuring $PGDATA/pg_hba.conf for master"
   cat << EOF >> $PGDATA/pg_hba.conf
@@ -87,7 +33,7 @@ archive_command = 'test ! -f $PGDATA/archive/%f && cp %p $PGDATA/archive/%f'
 max_wal_senders = 3
 max_replication_slots = 3
 #synchronous_standby_names = '${slave_node}'
-synchronous_standby_names = '${slave_host}'
+synchronous_standby_names = '$SYNCHONROUS_REPLICAS'
 #-------------------------
 # END K8S AUTO CONFIGURE
 #-------------------------
@@ -121,12 +67,9 @@ function configure_slave() {
 
   #configure the system
 
-  master_node=$(get_fqdn_at_index 0)
-  slave_host=$(get_host_at_index 1)
+  echo "Beginning bootstrap from host $MASTER_ENDPOINT"
 
-  echo "Beginning bootstrap from host ${master_node}"
-
-  gosu postgres pg_basebackup -D $PGDATA -p 5432 -U postgres -v -h ${master_node} --xlog-method=stream
+  gosu postgres pg_basebackup -D $PGDATA -p 5432 -U postgres -v -h $MASTER_ENDPOINT --xlog-method=stream
 
   if [ $? -ne 0 ]; then
     echo "FAILURE: Unable to restore from backup, existing"
@@ -150,7 +93,7 @@ EOF
 
   cat << EOF >> $PGDATA/recovery.conf
   standby_mode = on
-  primary_conninfo = 'host=${master_node} port=5432 user=postgres application_name=${slave_host}'
+  primary_conninfo = 'host=$MASTER_ENDPOINT port=5432 user=postgres application_name=$SYNCHONROUS_REPLICA'
 EOF
 
 
@@ -177,32 +120,11 @@ EOF
 }
 
 function configure_replica() {
-  echo "Configuring replica"
-  su -c "pg_ctl stop -D $PGDATA/" postgres
-
-  cp /var/lib/postgres/data/postgresql.conf /tmp/postgresql.conf
-
-  slave_node=$(get_fqdn_at_index 1)
-
-  su -c "pg_basebackup -D $PGDATA -p 5432 -U postgres -v -h ${slave_node} --xlog-method=stream" postgres
-
-
-    if [ $? -ne 0 ]; then
-      echo "FAILURE: Unable to restore from backup, existing"
-      exit 1
-    fi
-
-
-    #Make sure postgres owns everythign, or it will crap out
-    chown -R postgres  /var/lib/postgresql
-
-  # /postgres-config/posgres-agent replica configure --data $PGDATA/  --hostname $FQDN --port 5432 --user postgres --pg_conf $PGDATA/postgresql.conf
-  su -c "pg_ctl start -D $PGDATA/" postgres
+  echo "FAIL!  I need built"
 }
 
 
 #get the short hostname for comparison
-FQDN=$(hostname -f)
 #
 # HOSTNAME="foo-1"
 # FQDN="foo-1.bar.baz.local"
@@ -210,14 +132,15 @@ FQDN=$(hostname -f)
 
 echo "Configuring postres"
 echo "Hostname is $HOSTNAME"
-echo "FQDN is $FQDN"
-
+echo "FQDN is $(hostname -f)"
+echo "MEMBER_ROLE is $MEMBER_ROLE"
 #Make sure the postgres process owns the mounted directory
 
 
-if [[ "$HOSTNAME" == *-0 ]]; then
+
+if [[ "$MEMBER_ROLE" == "master" ]]; then
   configure_master
-elif [[ "$HOSTNAME" == *-1  ]]; then
+elif [[ "$MEMBER_ROLE" == "slave"  ]]; then
   configure_slave
 else
   configure_replica
